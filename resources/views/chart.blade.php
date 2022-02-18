7b7999ec8aeda44709ac9ff6f0380295903c259b
Nattawut <222-aiap@ap01.aisingroup.com>
2022-02-18 15:26:11 +0700

    oat commit2

--- resources/views/chart.blade.php
+++ resources/views/chart.blade.php
@@ -54,7 +54,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                     nattawut jaidee is the best
+                                     nattawut jaidee is the best uuuuuuuuu
                                 </div>
                             </div>
                         </div>
